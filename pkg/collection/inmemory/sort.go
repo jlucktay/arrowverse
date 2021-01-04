563012fa74995f4ff08f882cb8a99f73a7d399c2
James Lucktaylor <jlucktay+github@gmail.com>
2021-01-04 12:54:51 +0000

    feat(pkg/collection/inmemory): add regexes to aid sorting multi-episode/cross-show events

--- pkg/collection/inmemory/sort.go
+++ pkg/collection/inmemory/sort.go
@@ -1,6 +1,30 @@
 package inmemory
 
-import "go.jlucktay.dev/arrowverse/pkg/models"
+import (
+	"regexp"
+	"strings"
+
+	"go.jlucktay.dev/arrowverse/pkg/models"
+)
+
+type subexpressionName string
+
+func (s subexpressionName) String() string {
+	return string(s)
+}
+
+const (
+	multiPartNumber subexpressionName = "mpn"
+	multiPartTitle  subexpressionName = "mpt"
+)
+
+var (
+	rePart = regexp.MustCompile(`^(?P<` + multiPartTitle.String() + `>.*) Part (?P<` + multiPartNumber.String() +
+		`>[0-9a-zA-Z]+)$`)
+
+	numbers = strings.NewReplacer("One", "1", "Two", "2", "Three", "3", "Four", "4", "Five", "5", "Six", "6",
+		"Seven", "7", "Eight", "8", "Nine", "9")
+)
 
 // ByAirdate implements sort.Interface for []*models.Episode based on the Airdate field.
 type ByAirdate []models.Episode
@@ -9,9 +33,26 @@ func (a ByAirdate) Len() int      { return len(a) }
 func (a ByAirdate) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
 
 func (a ByAirdate) Less(i, j int) bool {
-	// If the two episodes aren't from the same show, then comparison of airdate is sufficient, for now. TODO?
+	// True if the two episodes aren't from the same show
 	if a[i].Season.Show.Name != a[j].Season.Show.Name {
-		return a[i].Airdate.Before(a[j].Airdate)
+		if !rePart.MatchString(a[i].Name) || !rePart.MatchString(a[j].Name) {
+			return a[i].Airdate.Before(a[j].Airdate)
+		}
+
+		// At this point, both episode titles match the regex and contain 'Part ...'
+
+		iMatches := rePart.FindStringSubmatch(a[i].Name)
+		jMatches := rePart.FindStringSubmatch(a[j].Name)
+
+		titleIndex := rePart.SubexpIndex(multiPartTitle.String())
+
+		// True if the multi-episode series has the same title, e.g. 'Crisis on Infinite Earths'
+		if iMatches[titleIndex] == jMatches[titleIndex] {
+			partNumberIndex := rePart.SubexpIndex(multiPartNumber.String())
+			return numbers.Replace(iMatches[partNumberIndex]) < numbers.Replace(jMatches[partNumberIndex])
+		}
+
+		// ...otherwise, fall through to remaining logic
 	}
 
 	// Otherwise ... TODO?
